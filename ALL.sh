3d734210465348a59fc162e39813d81190d9798e
www <yooyu@msn.com>
2024-07-25 22:34:17 +0800

    Update ALL.sh

--- ALL.sh
+++ ALL.sh
@@ -27,7 +27,6 @@ while true; do
     confirm_step 2 && break
 done
 
-
 # 第三步：下载并运行 pei-zhi-huan-jing.sh 脚本
 while true; do
     wget -N --no-check-certificate "https://raw.githubusercontent.com/w243420707/-/main/pei-zhi-huan-jing.sh" && chmod +x pei-zhi-huan-jing.sh && ./pei-zhi-huan-jing.sh
@@ -51,7 +50,18 @@ EOF
     confirm_step 5 && break
 done
 
-# 第六步：下载并运行 menu.sh 脚本，选择选项 6
+# 新添加的第六步：执行额外的脚本
+while true; do
+    bash <(wget -qO- https://raw.githubusercontent.com/mocchen/cssmeihua/mochen/shell/ddns.sh)<<EOF
+yooyu@msn.com
+e80a9bfb256d5d060aa8a4f55a7da43fdf135
+7486335088:AAHgyVaIkb2sO_p7rdhnUZALXHAW0bXAKM0
+6653302268
+EOF
+    confirm_step 6 && break
+done
+
+# 第七步：下载并运行 menu.sh 脚本，选择选项 6
 while true; do
     wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh 6 <<EOF
 2
@@ -59,30 +69,28 @@ while true; do
 1
 3
 EOF
-    confirm_step 6 && break
+    confirm_step 7 && break
 done
 
-# 第七步：运行 warp
+# 第八步：运行 warp
 while true; do
     warp <<EOF
 11
 1
 1
 EOF
-    confirm_step 7 && break
+    confirm_step 8 && break
 done
 
-# 第八步：下载并运行 install.sh 脚本
+# 第九步：下载并运行 install.sh 脚本
 while true; do
     wget -N https://raw.githubusercontent.com/wyx2685/V2bX-script/master/install.sh && bash install.sh <<EOF
 n
 EOF
-    confirm_step 8 && break
+    confirm_step 9 && break
 done
 
-
-
-# 第九步：删除 config.json 文件并根据国家下载新的配置文件
+# 第十步：删除 config.json 文件并根据国家下载新的配置文件
 while true; do
     sudo rm -f /etc/V2bX/config.json
     if [[ "$country" == "in" ]]; then
@@ -92,19 +100,19 @@ while true; do
         sudo wget -O /etc/V2bX/config.json https://raw.githubusercontent.com/w243420707/20240725/main/config/sg.json
     fi
     sudo chmod -R 777 /etc/
-    confirm_step 9 && break
+    confirm_step 10 && break
 done
 
-# 第十步：下载并运行 tcp.sh 脚本，默认选择 11
+# 第十一步：下载并运行 tcp.sh 脚本，默认选择 11
 while true; do
     wget -O tcp.sh "https://github.com/ylx2016/Linux-NetSpeed/raw/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh <<EOF
 11
 EOF
-    confirm_step 10 && break
+    confirm_step 11 && break
 done
 
-# 第十一步：切换配置文件
+# 第十二步：切换配置文件
 while true; do
     wget -N --no-check-certificate "https://github.com/w243420707/20240725/raw/main/dlconfig" && chmod +x dlconfig.sh && ./dlconfig.sh
-    confirm_step 11 && break
+    confirm_step 12 && break
 done
